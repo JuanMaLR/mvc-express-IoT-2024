7598f802de009416b24773c56e9dc770eaf52d00
JuanMaLR <A01206568@itesm.mx>
2024-10-01 17:34:35 -0600

    feat: prettier and eslint intial config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,9 @@ module.exports = {
       ecmaVersion: 'latest', // Configura la versión de ECMAScript que se utilizará. Aquí se establece en la versión más reciente.
     },
     extends: ['eslint:recommended', 'prettier'], // Extiende las reglas recomendadas de ESLint y las reglas de Prettier para formateo.
-    rules: {}, // Aquí puedes personalizar reglas adicionales, actualmente está vacío.
-    overrides: [] // Modifica las reglas recomendadas de eslint
+    plugins: ['prettier'], // Incluye el plugin de Prettier como parte de ESLiint
+    rules: {
+        'prettier/prettier': 'error', // Reporta errores de Prettier como errores de ESLint 
+    }, // Aquí puedes personalizar reglas adicionales
+    overrides: [] // Modifica las reglas recomendadas de ESLint
 };
