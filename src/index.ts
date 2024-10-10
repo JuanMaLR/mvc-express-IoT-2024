c119f00c4610950b95c33daacdd44e5cef7aeef2
JuanMaLR <A01206568@itesm.mx>
2024-10-10 16:31:44 -0600

    feat: creating first endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { config } from "dotenv";
 import express from "express";
+import userRouter from "./routes/user";
 
 //Para poder acceder a las variables del ambiente (.env)
 config();
 
 const app = express();
+
+app.use(userRouter);
 
 app.listen(process.env.SERVER_PORT, function () {
   console.log("Escuchando puerto " + process.env.SERVER_PORT);
