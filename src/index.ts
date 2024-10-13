fb578652320010f1c53de1eae5bf6de2a37afc69
JuanMaLR <A01206568@itesm.mx>
2024-10-13 12:35:38 -0600

    feat: adding 404 and error middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { config } from "dotenv";
 import express from "express";
 import studentRouter from "./routes/student";
+import testRoutes from "./routes/test";
+import unknownResource from "./middlewares/unknown-resource";
+import unknownError from "./middlewares/unknown-error";
 
 //Para poder acceder a las variables del ambiente (.env)
 config();
@@ -10,7 +13,17 @@ const app = express();
 // Middleware para parsear JSON
 app.use(express.json());
 
+// Rutas de la api
 app.use("/student", studentRouter);
+
+// Rutas de prueba
+app.use("/error", testRoutes);
+
+// Middlewares
+app.use(unknownResource); // Error 404, recurso no encontrado
+
+// Middlewares de error
+app.use(unknownError);
 
 app.listen(process.env.SERVER_PORT, function () {
   console.log("Escuchando puerto " + process.env.SERVER_PORT);
